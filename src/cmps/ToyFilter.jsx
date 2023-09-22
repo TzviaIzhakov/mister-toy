4cf060c9a89c87749ab81e5e334d655b4c9aee48
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-09-22 14:37:41 +0300

    fixing a bug considring the select of mui and the select of the react-select is the same name

--- src/cmps/ToyFilter.jsx
+++ src/cmps/ToyFilter.jsx
@@ -1,4 +1,6 @@
 import Select from 'react-select';
+import { FormControl, InputLabel, MenuItem, Select as SelectOPt} from "@mui/material"
+
 import { useEffect, useRef, useState } from "react"
 import { utilService } from "../services/util.service.js"
 import { toyService } from "../services/toy.service.js"
@@ -12,6 +14,7 @@ export function ToyFilter({ filterBy, onSetFilter }) {
  
     useEffect(() => {
         onSetFilter.current(filterByToEdit)
+        console.log(filterByToEdit,"filterByToEdit");
     }, [filterByToEdit])
 
 
@@ -46,6 +49,7 @@ export function ToyFilter({ filterBy, onSetFilter }) {
 
         <section className="car-filter full main-layout">
             <h2>Toys Filter</h2>
+            <section>
             <form >
                 <label htmlFor="name">Name:</label>
                 <input type="text"
@@ -56,13 +60,30 @@ export function ToyFilter({ filterBy, onSetFilter }) {
                     onChange={handleChange}
                 />
 
-                <label htmlFor="inStock"> By Stock:</label>
-                <input type="checkbox"
+                {/* <label htmlFor="inStock"> By Stock:</label> */}
+                {/* <input type="checkbox"
                     id="inStock"
                     name="inStock"
                     value={filterByToEdit.inStock}
                     onChange={handleChange}
-                />
+                /> */}
+                
+                  <FormControl fullWidth>
+                    <InputLabel id="inStock">By Stock:</InputLabel>
+                    <SelectOPt
+                    labelId="inStock"
+                    id="inStock"
+                    value={filterByToEdit.inStock}
+                    label="InStock"
+                    onChange={handleChange}
+                   name= 'inStock'
+                   >
+                    <MenuItem value={''}>All</MenuItem>
+                    <MenuItem value={true}>inStock</MenuItem>
+                    <MenuItem value={false}>Not In Stock</MenuItem>
+                  </SelectOPt>
+                </FormControl>
+
                 <label htmlFor="labels">By Labels:</label>
 
                 {/* <select name="labels" id="labels" onChange={handleChange}>
@@ -74,7 +95,7 @@ export function ToyFilter({ filterBy, onSetFilter }) {
                 />
 
             </form>
-
+            </section>
         </section>
     )
 }
