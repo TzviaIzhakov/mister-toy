688201d899ed08c02a11e4ad2f09d17232673de2
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-09-26 18:33:02 +0300

    F/trying to conver first the hedear to scss

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -4,13 +4,16 @@ import { NavLink } from 'react-router-dom'
 export function AppHeader() {
 
     return (
-        <header className="app-header">
-            <nav>
-                <NavLink to="/">Home</NavLink> |
-                <NavLink to="/toy">Toys</NavLink> |
-                <NavLink to="/about">About</NavLink> |
+        <header className="app-header main-layout full">
+            <nav className='main-nav-container grid space-between align-center flow-column'>
+            <div>LOGO</div>
+            <ul className='main-nav clean-list grid flow-column'>
+            <li><NavLink to="/">Home</NavLink> </li>
+            <li> <NavLink to="/toy">Toys</NavLink> </li>
+            <li> <NavLink to="/about">About</NavLink> </li>
+            </ul>
+               
             </nav>
-            <h1>My App</h1>
         </header>
     )
 }
